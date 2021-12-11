ba64adcb7a2cc1d363984ba473c005bb230807b0
Guilherme Lazari <guilhermelcs@gmail.com>
2021-12-11 17:50:44 -0300

    Add support for parsing array

--- rustjson/src/main.rs
+++ rustjson/src/main.rs
@@ -5,6 +5,6 @@ fn main() {
     let out = lexer::lex("1 2 \"asdf\"", 0);
     println!("'{:?}", out);
 
-    let out = parser::parse("12");
+    let out = parser::parse("[\"adsf\", 1, 2, [3,4]]");
     println!("'{:?}", out);
 }
--- rustjson/src/parser.rs
+++ rustjson/src/parser.rs
@@ -4,20 +4,25 @@ use crate::lexer::{self, Token};
 pub enum Value<'a> {
     NUM(f32),
     STR(&'a str),
+    ARR(Vec<Value<'a>>),
 }
 
 #[derive(Debug, PartialEq, Clone)]
 enum Error {
     NotANumber(String),
+    UnexpectedToken(String),
+    ExpectedValueAfterComma,
+    UnclosedArray,
+    E,
 }
 
 pub fn parse(string: &str) -> Value {
     let tokens = lexer::lex(string, 0);
-    let (val, _) = parse_value(tokens, 0).expect("Invalid Json");
+    let (val, _) = parse_value(&tokens, 0).expect("Invalid Json");
     return val;
 }
 
-fn parse_value(tokens: Vec<Token>, i: usize) -> Result<(Value, usize), Error> {
+fn parse_value<'a>(tokens: &Vec<Token<'a>>, i: usize) -> Result<(Value<'a>, usize), Error> {
     let token = &tokens[i];
     match token {
         Token::NUM(_) => match parse_num(token) {
@@ -25,7 +30,11 @@ fn parse_value(tokens: Vec<Token>, i: usize) -> Result<(Value, usize), Error> {
             Err(e) => Err(e),
         },
         Token::STRING(s) => Ok((Value::STR(s), i + 1)),
-        _ => panic!("Invalid token: {:?}", token),
+        Token::LBRACKET => match parse_array(tokens, i) {
+            Ok((arr, i)) => Ok((Value::ARR(arr), i)),
+            Err(e) => Err(e),
+        },
+        _ => panic!("Unexpected token: {:?}", token),
     }
 }
 
@@ -35,6 +44,52 @@ fn parse_num<'a>(t: &'a Token<'a>) -> Result<f32, Error> {
         _ => return Err(Error::NotANumber(t.to_string())),
     };
     return s.parse().map_err(|_| Error::NotANumber(t.to_string()));
+}
+
+fn parse_array<'a>(tokens: &Vec<Token<'a>>, i: usize) -> Result<(Vec<Value<'a>>, usize), Error> {
+    let (mut out, i) = (vec![], i + 1);
+
+    if tokens[i] == Token::RBRACKET {
+        return Ok((out, i + 1));
+    }
+
+    let (val, i) = parse_value(tokens, i)?;
+    out.push(val);
+    let mut loop_i = i;
+    while loop_i < tokens.len() && tokens[loop_i] != Token::RBRACKET {
+        if tokens[loop_i] != Token::COMMA {
+            return Err(Error::UnexpectedToken(tokens[loop_i].to_string()));
+        }
+        loop_i += 1;
+        if loop_i >= tokens.len() {
+            return Err(Error::ExpectedValueAfterComma);
+        }
+        let (val, i) = parse_value(tokens, loop_i)?;
+        out.push(val);
+        loop_i = i;
+    }
+    if loop_i >= tokens.len() {
+        return Err(Error::UnclosedArray);
+    }
+
+    return Ok((out, loop_i + 1));
+}
+
+#[cfg(test)]
+#[test]
+fn test_array() {
+    let tests = [
+        ("[]", vec![], 2),
+        ("[1]", vec![Value::NUM(1.)], 3),
+        ("[1,2]", vec![Value::NUM(1.), Value::NUM(2.)], 5),
+    ];
+
+    for test in tests.iter() {
+        let (e, out, i) = test;
+        let (got, got_i) = parse_array(&lexer::lex(e, 0), 0).expect("test case failed");
+        assert_eq!(*out, got, "{}", e);
+        assert_eq!(*i, got_i, "{}", e);
+    }
 }
 
 #[cfg(test)]
@@ -58,6 +113,10 @@ fn test_parse() {
         ("21", Value::NUM(21.)),
         ("123", Value::NUM(123.)),
         (" \"asdf\"  ", Value::STR("asdf")),
+        (
+            "[1,2,3]",
+            Value::ARR(vec![Value::NUM(1.), Value::NUM(2.), Value::NUM(3.)]),
+        ),
     ];
 
     for test in tests.iter() {
